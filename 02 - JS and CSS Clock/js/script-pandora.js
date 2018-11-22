0107ea518e2a7cf523b1e9023a0636d480e1ee93
ilyana19 <aries_2152@hotmail.com>
2018-11-22 13:09:07 -0500

    changing to css sprite sheet animation instead

--- 02 - JS and CSS Clock/js/script-pandora.js
+++ 02 - JS and CSS Clock/js/script-pandora.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", function(event) {
   const hand = document.querySelector(".hand");
+  const originalHand = document.querySelector(".original-hand");
+
+  // function setDate() {
+  //   const now = new Date();
+  //   const seconds = now.getSeconds();
+  //   const secondsDegrees = ((seconds / 60) * 360) + 90;
+  //   // hand.style.transform = `rotate(${secondsDegrees}deg)`;
+  //   // if (seconds === 0) hand.classList.add("transition-fix");
+  //   // else hand.classList.remove("transition-fix");
+  //
+  //   originalHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  //   if (seconds === 0) originalHand.classList.add("transition-fix");
+  //   else originalHand.classList.remove("transition-fix");
+  // }
+  //
+  // setInterval(setDate, 1000);
+
+  function rotateClock() {
+    hand.style.transform = 'rotate(45deg)';
+  }
+
+  // rotateClock();
 });
